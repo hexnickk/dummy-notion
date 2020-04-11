a3493d84c6a6c5f3f62857e97f8610e7e8c76238
Kozlov Nikita <kozlovzxc@gmail.com>
2020-04-11 21:00:37 +0300

    feat(todos): simple todos form

--- src/components/todos-page/todos-page.component.tsx
+++ src/components/todos-page/todos-page.component.tsx
@@ -1,21 +1,34 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { useStore } from 'effector-react';
-import { todosStore } from '~src/stores/todos';
+import { addTodo, todosStore } from '~src/stores/todos';
 import { TodosListComponent } from './todos-list.component';
 
-// const NewTodoForm = () => {
-//     return (
-//         <div>
-//             <input></input>
-//             <button></button>
-//         </div>
-//     );
-// };
+const TodoForm = () => {
+    const [title, setTitle] = useState('');
+    const onSubmit = (event: Event) => {
+        event.preventDefault();
+        addTodo({
+            title,
+        })
+    };
+
+    return (
+        <form onSubmit={onSubmit}>
+            <input
+                placeholder="What todo?"
+                value={title}
+                onChange={(e) => setTitle(e.target.value)}
+            ></input>
+            <button type="submit">Add Todo!</button>
+        </form>
+    );
+};
 
 const TodosPage = () => {
     const todos = useStore(todosStore);
     return (
         <div>
+            <TodoForm></TodoForm>
             <TodosListComponent todos={todos}></TodosListComponent>
         </div>
     );
